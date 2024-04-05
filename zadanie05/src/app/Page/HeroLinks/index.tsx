35167a89e002aa6a29df6ccf64dfb22b02edf1d0
Krzysztof Kwiatkowski <kestivvi@gmail.com>
2024-04-05 19:34:12 +0200

    Add href to hero links

--- zadanie05/src/app/Page/HeroLinks/index.tsx
+++ zadanie05/src/app/Page/HeroLinks/index.tsx
@@ -1,19 +1,32 @@
-import styles from "./styles.module.css"
+import styles from "./styles.module.css";
 import Image from "next/image";
 import playButtonIcon from "@/app/img/play-button.svg";
 import rightArrowIcon from "@/app/img/right-arrow.svg";
+import Link from "next/link";
 
 export default function HeroLinks() {
   return (
     <div className={styles.hero_links}>
-      <div className={`${styles.link} ${styles.accent_link}`}>
-        <Image className={styles.play_button} src={playButtonIcon} alt="play button" />
-        Why Moon Lake?
-      </div>
-      <div className={styles.link}>
-        Get Started
-        <Image className={styles.right_arrow} src={rightArrowIcon} alt="right arrow" />
-      </div>
+      <Link href="/about">
+        <div className={`${styles.link} ${styles.accent_link}`}>
+          <Image
+            className={styles.play_button}
+            src={playButtonIcon}
+            alt="play button"
+          />
+          Why Moon Lake?
+        </div>
+      </Link>
+      <Link href="/docs">
+        <div className={styles.link}>
+          Get Started
+          <Image
+            className={styles.right_arrow}
+            src={rightArrowIcon}
+            alt="right arrow"
+          />
+        </div>
+      </Link>
     </div>
-  )
+  );
 }
